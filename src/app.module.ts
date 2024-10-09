392f56b605843c7ce770084dc531eb1e3666fd39
Arturo Bernal <arturo@email.com>
2024-10-09 10:53:47 +0200

    app dockerizada

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import mikroOrmConfig from './mikro-orm.config';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './app.config';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(mikroOrmConfig), RestaurantsModule],
+  imports: [
+    MikroOrmModule.forRoot(mikroOrmConfig),
+    RestaurantsModule,
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -4,12 +4,12 @@ import { MariaDbDriver } from '@mikro-orm/mariadb';
 export default {
   entities: ['dist/entities'], // compiled JS files
   entitiesTs: ['src/entities'],
-  dbName: 'assbook_lite',
+  dbName: process.env.DB_DATABASE || 'assbook_lite',
   driver: MariaDbDriver,
   driverOptions: { connection: { timezone: '+02:00' } },  
-  user: 'root',
-  password: '',
-  port: 3306,
-  host: 'localhost',
+  user: process.env.DB_USERNAME || 'example',
+  password: process.env.DB_PASSWORD || 'example',
+  port: parseInt(process.env.DB_SERVER_PORT, 10) || 3306,
+  host: process.env.DB_SERVER_HOST || 'localhost',
   debug: true,
 } as ConnectionOptions;
